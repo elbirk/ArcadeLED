4c0f89d8baf60d1208c5dea62cf70d3eb9a5f2aa
elbirk <54961605+elbirk@users.noreply.github.com>
2019-12-24 01:57:33 +0100

    Add files via upload

---
+++ ArcadeLED neopixel/runcommand-onend.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+# Turn off all LED.
+sudo python3 /home/pi/RetroPie/ArcadeLED/ArcadeLED.py -m neopixel
+
---
+++ ArcadeLED neopixel/runcommand-onstart.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+if [ -z "$3" ]; then
+  exit 0
+fi
+
+rom_name="$(basename "$3")"
+rom="${rom_name%.*}"
+
+# Turn on the relevant LEDs for this game.
+sudo python3 /home/pi/RetroPie/ArcadeLED/ArcadeLED.py -r "$rom" -m neopixel
