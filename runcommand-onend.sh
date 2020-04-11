2e5084346f462ba0d924cd7d0a08cdfd1b2af6bc
elbirk <54961605+elbirk@users.noreply.github.com>
2020-04-11 12:48:17 +0200

    Delete runcommand-onend.sh

--- runcommand-onend.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-# Turn off all neopixel.
-sudo python3 /home/pi/RetroPie/ArcadeLED/ArcadeLED.py -m neopixel
-
-# Turn off all LED.
-#sudo python3 /home/pi/RetroPie/ArcadeLED/ArcadeLED.py -r OFF
